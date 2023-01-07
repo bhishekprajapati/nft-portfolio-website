0c1ec71b24448489a730afd5ad9141ecde3c4777
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-01-07 07:08:17 +0530

    configure tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    colors: {
+      white: '#fff',
+      black: '#000',
+      navy: '#051139',
+      blue: '#1e50ff',
+      purple: '#aa00ff',
+      cyan: '#5699ff',
+      'gary-dark': '#0e0e0e',
+      'blue-dark': '#081956',
+      // for text
+      'color-title': '#333',
+      'color-body': '#666',
+      'color-field': '#b8b8b8',
+      'color-disable': '#ebebeb',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
